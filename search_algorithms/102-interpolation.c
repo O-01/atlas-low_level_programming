dc82c3be5992509cbb5505bc05a9aa7670867c5d
O-01 <sa.an-s@yandex.com>
2024-08-27 13:34:39 -0500

    Saving progress on 102-interpolation - local testing successful (search_algorithms)

---
+++ search_algorithms/102-interpolation.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - searches for value in sorted array of integers using
+ *                        interpolation search algorithm
+ * @array: pointer to first element of array to search
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: first index where value is located
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t lo = 0, hi = size - 1, pos = 0;
+
+	if (!array || !size)
+		return (-1);
+	for (pos = POS(array, lo, hi, value);
+		lo < hi && value > array[lo] && value < array[hi];
+		pos = POS(array, lo, hi, value))
+	{
+		printf("Value checked array[%lu] = [%u]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		else if (array[pos] < value)
+			lo = pos;
+		else if (array[pos] > value)
+			hi = pos;
+	}
+	printf("Value checked array[%lu] is out of range\n", pos);
+	return (-1);
+}
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define POS(a, l, h, v) \
+	(l + (((double)((h) - (l)) / ((a)[(h)] - (a)[(l)])) * ((v) - (a)[(l)])))
+
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+
+#endif
